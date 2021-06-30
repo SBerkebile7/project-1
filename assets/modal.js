0eb4979249153fb9e1adbc9a1dea2d27e1109ff8
Emily N Mayeski <enmayeski@gmail.com>
2021-06-29 21:01:54 -0400

    got most of the CSS and functionality working.

---
+++ assets/modal.js
@@ -0,0 +1,13 @@
+// MODAL FUNCTIONS
+
+$("#new-rec").click(function() {
+  $(".modal").css("display", "flex");
+});
+
+$("#close-modal").click(function() {
+  $(".modal").css("display", "none");
+});
+
+$("#cancel-modal").click(function() {
+  $(".modal").css("display", "none");
+});

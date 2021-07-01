5d6c6a5f58e9d37f4711a4a12a5bbd0fc0d9d910
Spenny Berks <sberkebile@live.com>
2021-07-01 17:00:53 -0400

    added push to screen for choice of trip, working on button functionality for pulling up of previous trip

--- assets/gmaps.js
+++ assets/gmaps.js
@@ -1,6 +1,12 @@
 var directionService = new google.maps.DirectionsService();
 var directionsDisplay = new google.maps.DirectionsRenderer();
 
+var allTrips = [{
+    start: "",
+    destination: "",
+    type: ""
+}];
+
 // This function calculates the route that would be taken based on a begin point and an end point
 function calcRoute() {
     // request info pulled from modal
@@ -15,8 +21,8 @@ function calcRoute() {
     // uses google maps DirectionsService to calculate a route
     directionService.route(request, (result, status) => {
         if(status == google.maps.DirectionsStatus.OK) {
-            //const output = document.querySelector('#output');
-            //output.innerHTML = "<div>From: " + document.getElementById("beginPoint").value + ". <br />To: " + document.getElementById("endPoint").value + ".<br/>Driving distance: " + result.routes[0].legs[0].distance.text + ". <br />Duration: " + result.routes[0].legs[0].duration.text + "</div>";
+            const output = document.querySelector('#outputTrip');
+            output.innerHTML = "<div>From: " + document.getElementById("beginPoint").value + ". <br />To: " + document.getElementById("endPoint").value + ".<br/>Driving distance: " + result.routes[0].legs[0].distance.text + ". <br />Duration: " + result.routes[0].legs[0].duration.text + "</div>";
             console.log("Going from " + document.getElementById("beginPoint").value + " to " + document.getElementById("endPoint").value)
             console.log("is " + result.routes[0].legs[0].distance.text + " and will take you " + result.routes[0].legs[0].duration.text + " via " + document.getElementById("mode").value);
             directionsDisplay.setDirections(result);
@@ -27,6 +33,10 @@ function calcRoute() {
             output.innerHTML = "<div>Could not retrieve driving distance.</div>";
         }
     });
+
+    $("#beginPoint").val("");
+    $(".modal").css("display", "none");
+    saveAndStore();
 }
 
 var options = {
@@ -38,3 +48,79 @@ var autocompleteBegin = new google.maps.places.Autocomplete(beginInput, options)
 
 var endInput = document.getElementById("endPoint");
 var autocompleteEnd = new google.maps.places.Autocomplete(endInput, options);
+
+// Lists trips taken on each tab based on their respective trip type
+function listTripAll() {
+    $("#prev-suggestions-all").empty();
+    
+    allTrips.forEach(function(allTrips) {
+        console.log("All: " + allTrips);
+        $("#prev-suggestions-all").append($(`<button class='list-group-item chosenTrip'>${allTrips}</button>`))
+    })
+}
+
+// Lists trips taken on each tab based on their respective trip type
+function listTripDriving() {
+    $("#prev-suggestions-driving").empty();
+
+    allTrips.forEach(function(allTrips) {
+        console.log("Driving: " + allTrips);
+        $("#prev-suggestions-driving").append($(`<button class='list-group-item chosenTrip'>${allTrips}</button>`))
+    })
+}
+
+// Lists trips taken on each tab based on their respective trip type
+function listTripBicycling() {
+    $("#prev-suggestions-biking").empty();
+
+    allTrips.forEach(function(allTrips) {
+        console.log("Bicycling: " + allTrips);
+        $("#prev-suggestions-biking").append($(`<button class='list-group-item chosenTrip'>${allTrips}</button>`))
+    })
+}
+
+// Lists trips taken on each tab based on their respective trip type
+function listTripTransit() {
+    $("#prev-suggestions-public").empty();
+
+    allTrips.forEach(function(allTrips) {
+        console.log("Transit: " + allTrips);
+        $("#prev-suggestions-public").append($(`<button class='list-group-item chosenTrip'>${allTrips}</button>`))
+    })
+}
+
+// Lists trips taken on each tab based on their respective trip type
+function listTripWalking() {
+    $("#prev-suggestions-walking").empty();
+
+    allTrips.forEach(function(allTrips) {
+        console.log("Walking: " + allTrips);
+        $("#prev-suggestions-walking").append($(`<button class='list-group-item chosenTrip'>${allTrips}</button>`))
+    })
+}
+
+// Stores trips to localStorage
+function storeTrip() {
+    localStorage.setItem("trips", JSON.stringify(allTrips));
+}
+
+// Finds and saves the trip based on it's selected type of route
+function saveAndStore() {
+    console.log('saveAndStore was run');
+
+    var currentTrip = $("#beginPoint").val() || '';
+    allTrips.push(currentTrip);
+
+    listTripAll();
+    storeTrip();
+    
+    if(document.getElementById("mode").value == "DRIVING") {
+        listTripDriving();
+    } else if(document.getElementById("mode").value == "BICYCLING") {
+        listTripBicycling();
+    } else if(document.getElementById("mode").value == "TRANSIT") {
+        listTripTransit();
+    } else if(document.getElementById("mode").value == "WALKING") {
+        listTripWalking();
+    }
+};

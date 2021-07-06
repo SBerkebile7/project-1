512da7632529876104ad6bb354d4a18e89ddcece
Raevin Dunson <raevin.dunson98@gmail.com>
2021-07-06 15:01:45 -0400

    book movie api test

--- assets/nytimes.js
+++ assets/nytimes.js
@@ -1,5 +1,5 @@
 fetch(
-  'https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=HXMcUKu4hWhoZPQBW99bGZ9An0FdbWEl'
+  'https://api.nytimes.com/svc/books/v3/lists/current/e-book-fiction.json?api-key=HXMcUKu4hWhoZPQBW99bGZ9An0FdbWEl'
 )
 .then(function(res) {
   return res.json();
@@ -7,3 +7,40 @@ fetch(
 .then(function(data) {
   console.log(data);
 });
+
+//fetch (
+//  'https://api.nytimes.com/svc/books/v3/reviews.json?' +
+//  'title='
+//  '&api-key=HXMcUKu4hWhoZPQBW99bGZ9An0FdbWEl'
+//)
+//.then(function(res) {
+//  return res.json();
+//})
+//.then(function(data) {
+//  console.log(data);
+//});
+
+fetch(
+  'https://api.nytimes.com/svc/movies/v2/reviews/search.json?' +
+  '&api-key=HXMcUKu4hWhoZPQBW99bGZ9An0FdbWEl'
+)
+.then(function(res) {
+  return res.json();
+})
+.then(function(data) {
+  console.log(data);
+});
+
+var recAmount = 10;
+
+function displayRecommendations(data) {
+
+  $("#url-return").empty();
+
+  for (var i = 0; i < recAmount; i++) {
+
+  var urlReturned = data.docs[i].web_url;
+  $('#reviews').append('<div id="url-return"></div>');
+  $("#url-return").append(`<a href="${urlReturned}" target="_blank">${articleName}</a><br/>`);
+  }
+}

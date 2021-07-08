949b36adff2eab526c377637a41222474af8f72b
Raevin Dunson <raevin.dunson98@gmail.com>
2021-07-08 17:02:40 -0400

    added warning modal with checkbox

--- assets/warning.js
+++ assets/warning.js
@@ -3,3 +3,11 @@
 $("#close-warning").click(function() {
     $(".modal").css("display", "none");
   });
+
+var checkbox = document.getElementById("checkbox")
+
+$("#proceed").click(function() {
+    if (checkbox.checked == true) {
+        $(".modal").css("display", "none");
+    }
+});

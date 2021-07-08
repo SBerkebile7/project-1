fb128b393387784a2ed4fa87a8273be1e0a2ad7c
Spenny Berks <sberkebile@live.com>
2021-07-08 14:38:26 -0400

    Made columns for trip and articles split so they show side-by-side instead of in a single column. Also set columns to be vertically centered so it fills the page a bit better

--- assets/gmaps.js
+++ assets/gmaps.js
@@ -46,8 +46,6 @@ function calcRoute() {
         unitSystem: google.maps.UnitSystem.IMPERIAL
     }
 
-    
-
     // uses google maps DirectionsService to calculate a route
     directionService.route(request, (result, status) => {
         if(status == google.maps.DirectionsStatus.OK) {
@@ -317,7 +315,7 @@ function popularArticles() {
     var articleName = data.docs[i].headline.main
     // console.log(res.response.docs[0].web_url);
     // console.log(res.response.docs[0].headline.main);
-    $('#outputTrip').append('<div id="url-return"></div>');
+    $('#outputArticles').append('<div id="url-return"></div>');
     $("#url-return").append(`<a href="${urlReturned}" target="_blank">${articleName}</a><br/>`);
 
     }
@@ -344,7 +342,7 @@ function popularArticles() {
         for (var i = 0; i < 5; i++) {
             var recsURL = res.results.books[i].amazon_product_url;
             var bookTitle = res.results.books[i].title;
-            $('#outputTrip').append('<div id="recs"></div>');
+            $('#outputArticles').append('<div id="recs"></div>');
             $("#recs").append(`<a href="${recsURL}" target="_blank">${bookTitle}</a><br/>`);
         }
       }
